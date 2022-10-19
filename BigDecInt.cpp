0d2c9865b1571b598ff6e7bb4c62aa414cdcde20
__Rawan__ <rawanroni2003@gmail.com>
2022-10-19 23:23:32 +0200

    Added Setter and Getter for sign attribute and CheckSize function

--- BigDecInt.cpp
+++ BigDecInt.cpp
@@ -8,3 +8,37 @@ void BigDecimalInt::setVec (vector <int> v){
 vector<int> BigDecimalInt::getVector (){
     return vec;
 }
+
+void BigDecimalInt::setS(char sign){
+    s = sign;
+}
+
+char BigDecimalInt::getS(){
+    return s;
+}
+
+void BigDecimalInt::checkSize(vector <int>& v1,vector <int>& v2){
+    int z = 0;
+    if (v1.size() > v2.size() ) {
+        vector<int> v3 = v2;
+        z = (v1.size() - v2.size());
+        v2.clear();
+        for (int i = 0; i < z; i++) {
+            v2.push_back(0);
+        }
+        for (int i = 0; i < v3.size(); i++) {
+            v2.push_back(v3[i]);
+        }
+    }
+    else if(v1.size() < v2.size()){
+        vector<int> v3 = v1;
+        z = (v2.size() - v1.size());
+        v1.clear();
+        for (int i = 0; i < z; i++) {
+            v1.push_back(0);
+        }
+        for (int i = 0; i < v3.size(); i++) {
+            v1.push_back(v3[i]);
+        }
+    }
+}
