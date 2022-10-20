f72d45153652803f72d4312977605e38b26a1111
AsmaaElawady <101433174+AsmaaElawady@users.noreply.github.com>
2022-10-20 20:15:10 +0200

    added main

--- GroupTask.cpp
+++ GroupTask.cpp
@@ -1,3 +1,4 @@
+//Asmaa Elawady Ibrahim    20210068
 #include <bits/stdc++.h>
 #include"BigDecInt.h"
 #include "BigDecInt.cpp"
@@ -5,6 +6,38 @@
 using namespace std;
 
 int main() {
-   
+    string num1, num2 ;
+    cout << "***********Hello***********\n";
+    cout << "***********Welcome To Big Integers Calculator***********\n";
+    cout << "Please enter the first number = " ;
+    getline(cin, num1);
+    while(!(regex_match(num1,regex("[+-]?[0-9]+")))){
+        cout << "NOT VALID, Please enter a valid number = ";
+        getline(cin, num1);
+    }
+    BigDecimalInt b(num1) ;
+    cout << "Please enter the second number = " ;
+    getline(cin, num2);
+    while(!(regex_match(num2,regex("[+-]?[0-9]+")))){
+        cout << "NOT VALID, Please enter a valid number = ";
+        getline(cin, num2);
+    }
+    BigDecimalInt b1(num2);
+    cout << " Operator+ = " << b + b1 << endl ;
+    if(b == b1) {
+        cout << "The Two numbers are equal.\n";
+    }else {
+        cout << "The Two numbers are not equal.\n";
+    }
+    if (b > b1) {
+        cout << "First number is greater than Second number\n";
+    } else if (b == b1) {
+        cout << "First number equals Second number.\n";
+    } else if(b < b1) {
+        cout << "First number is smaller than Second number\n";
+    }
+    cout <<" Operator- = "<<b1 - b<<endl;
+    BigDecimalInt b2 = b1;
+    cout << "Assignment operator (new number = second number) = " << b2;
 }
 
